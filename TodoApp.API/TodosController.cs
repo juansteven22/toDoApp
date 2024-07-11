09e16f9f03844d233765274dca76bb52354eab67
unknown <jusmontenegrope@unal.edu.co>
2024-07-10 21:40:09 -0500

    initial commit

---
+++ TodoApp.API/TodosController.cs
@@ -0,0 +1,25 @@
+[ApiController]
+[Route("api/[controller]")]
+public class TodosController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public TodosController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Todo>>> GetTodos()
+    {
+        return await _context.Todos.ToListAsync();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Todo>> PostTodo(Todo todo)
+    {
+        _context.Todos.Add(todo);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetTodos), new { id = todo.Id }, todo);
+    }
+}
